757c9c77cc714d5c33bd23bbdd91f5d0269d06ab
Fernando Carril <facarril@gmail.com>
2026-03-19 16:25:53 +0100

    remove path import, keep only fs/promises

--- packages/utils/src/filesystem.ts
+++ packages/utils/src/filesystem.ts
@@ -1,6 +1,5 @@
 import { readFile } from 'fs/promises'
-import path from 'path'
 
 export async function readRepoFile(filePath: string): Promise<string> {
-  return readFile(path.resolve(process.cwd(), filePath), 'utf-8')
+  return readFile(filePath, 'utf-8')
 }
